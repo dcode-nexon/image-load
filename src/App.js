31e41d03eeebf846f80a7333d4443bf9546ea9cf
tubi55 <tubi55@nate.com>
2022-12-09 18:48:48 +0900

    리액트에서 이미지 불러오기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './scss/style.scss';
 
+import pic1 from './img/Blizzards.jpg';
+import pic2 from './img/Calm.jpg';
+import pic3 from './img/Dusty_Road.jpg';
+
+const imgs = [pic1, pic2, pic3];
+
 function App() {
-	//src폴더안쪽에서 public폴더까지의 절대값 경로를 구하는 구문
-	const path = process.env.PUBLIC_URL;
 	return (
 		<div className='wrap'>
-			<img src={path + '/img/Blizzards.jpg'} alt='Blizzards' />
-			<img src={path + '/img/Calm.jpg'} alt='Calm' />
-			<img src={path + '/img/Dusty_Road2.jpg'} alt='Dusty_Road' />
+			{imgs.map((pic, idx) => (
+				<img src={pic} alt={pic} key={idx} />
+			))}
 		</div>
 	);
 }
